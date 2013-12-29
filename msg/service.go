cc6527d7343e4b8b79ef39beaf21698f68bf1927
Miek Gieben <miek@miek.nl>
2013-12-29 21:44:36 +0000

    remove newline

--- msg/service.go
+++ msg/service.go
@@ -20,13 +20,11 @@ type Service struct {
 // Returns the amount of time remaining before expiration
 func (s *Service) RemainingTTL() uint32 {
 	d := s.Expires.Sub(time.Now())
-
 	ttl := uint32(d.Seconds())
 
 	if ttl < 1 {
 		return 0
 	}
-
 	return ttl
 }
 
