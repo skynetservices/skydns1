f143a6576bc366785e8c615d65c706cb96221c89
Miek Gieben <miek@miek.nl>
2013-12-29 20:09:57 +0000

    Add Callback to Service
    Some groundwork for callback, callback will be separately put in the
    registry, but linked from each Service.

--- msg/service.go
+++ msg/service.go
@@ -18,6 +18,7 @@ type Service struct {
 	Port        uint16
 	TTL         uint32 // Seconds
 	Expires     time.Time
+	Callback    []string `json:"-"` // Callbacks are found by UUID
 }
 
 // Returns the amount of time remaining before expiration
@@ -36,4 +37,18 @@ func (s *Service) RemainingTTL() uint32 {
 // Updates TTL property to the RemainingTTL
 func (s *Service) UpdateTTL() {
 	s.TTL = s.RemainingTTL()
+}
+
+type Callback struct {
+	UUID        string
+
+	// Name of the service
+	Name        string
+	Version     string
+	Environment string
+	Region      string
+	Host        string
+
+	Reply	    string
+	Port        uint16
 }
